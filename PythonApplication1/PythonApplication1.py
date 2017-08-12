826c274294a2f464f3953f55db5fc1645397e65e
Isuuuu <490093640@qq.com>
2017-08-12 16:30:34 +0800

    update By ZJl

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 (just for NOtes)-*
 import getpass
+from test.double_const import PI
 #name="first to write py"
 #print name
 #print('Hello World')
@@ -12,3 +13,14 @@ if a>=0:
     print(a)
 else:
     print(-a)
+
+print 'abc\r\ndef'
+print r'\r\n'   #r''中的字符串均不转义
+print('''line1
+line2
+line3''')  #打印出3行
+
+print "a"=="A"
+print PI
+print 10/3
+print 10//3
