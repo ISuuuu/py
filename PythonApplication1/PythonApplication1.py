8d1fcd92e81aeb33b619a1d9253e06247478fd6b
Isuuuu <490093640@qq.com>
2017-08-12 15:09:38 +0800

    commit

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 (just for NOtes)-*
+import getpass
+#name="first to write py"
+#print name
+#print('Hello World')
+#names=raw_input("input userdi")
+#print name 
+#pwd = getpass.getpass("ren")
+#print pwd
+a=100
+if a>=0:
+    print(a)
+else:
+    print(-a)
