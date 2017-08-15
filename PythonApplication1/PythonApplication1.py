0a012d392b269e79f86d2989035a5a1a83f8a088
Isuuuu <490093640@qq.com>
2017-08-15 22:11:31 +0800

    Daily Update By Zjl 2017/08/15

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 (just for NOtes)-*
+# -*- coding: utf-8 -*-
 import getpass
 from test.double_const import PI
 #name="first to write py"
@@ -24,3 +24,18 @@ print "a"=="A"
 print PI
 print 10/3
 print 10//3
+name="中文输入"
+if isinstance(name, unicode):
+    print name.encode('utf-8')
+else: 
+    print name.decode('utf-8')
+print u'\u54c8\u54c8'  #中文正常
+print ord('A')
+#print ord('中').encode('utf-8')
+print chr(86)
+
+#############数组
+classmates=['a1','a2',PI]
+print classmates
+print classmates[0]
+print classmates[-1] #索引  获取最后一个元素
