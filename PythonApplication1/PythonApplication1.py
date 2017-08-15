fbdc3e4f5fa5ee448706cb82b1a501df3ad46b09
Isuuuu <490093640@qq.com>
2017-08-15 23:06:38 +0800

    DailyUpdate By zjl 2017/08/15

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -39,3 +39,39 @@ classmates=['a1','a2',PI]
 print classmates
 print classmates[0]
 print classmates[-1] #索引  获取最后一个元素
+classmates.append('ByAppent')
+classmates.insert(0,'ByInsert')
+classmates.pop(1) #删除指定的位置的元素  参数为空 删除末尾元素
+print classmates
+
+classM=('a1','a2','a3')  #值一旦确定，就无法赋值 ,为空t=(), 
+classM2=(1)  #表示int 1
+print classM2
+classM3=(1,) #表示仅有一个元素
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+print L[0][0]
+
+str=1981
+print str
+print int(str)
+
+sum=0
+for x in [1,2,3,4,5,6,7,8,9]:
+    sum+=x
+print sum
+
+#classmates.append(range(5)) 
+classM4=range(5)
+for x in classM4:
+    classmates.append(x)
+print classM4
+print classmates
+
+d={'a1':88,'a2':98,'a3':60}
+print d['a1']
