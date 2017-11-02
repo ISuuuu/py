36d17ac9ae5a68b78a8d3dac48c035c13c3f8541
ISuuuu <490093640@qq.com>
2017-11-02 17:11:29 +0800

    new1

---
+++ PythonApplication1/a.py
@@ -0,0 +1,23 @@
+L=['name1','name2','name2','name3']
+
+print L[0]
+
+L1=[]
+n=4
+for i in range(n):
+    L1.append(L[i])
+
+print L1[0]
+#python 提供了切片（Slice）操作符，简化操作
+print L1[0:3]  #从第一个参数 开始索引位置（第二个参数），但不包括
+print L1[1:3]
+print L1[-1:]  #倒数第一个元素
+L1=[]
+
+L1=list(range(100))
+
+L1[10:20] #即 取值 11-20
+
+print  L1[:10:4]  # [0:10] 取值0--10，
+
+print L1[::5]
